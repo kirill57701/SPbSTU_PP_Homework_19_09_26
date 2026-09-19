b2cead56f1a662da50bfb7615dde85d8554b1067
Kirill <kirillpetrov56333@gmail.com>
2026-09-19 22:25:33 +0300

    adding area

--- main.cpp
+++ main.cpp
@@ -35,6 +35,36 @@ void* worker(void* arg)
   return nullptr;
 }
 
+double area(double r, size_t threads, size_t tests) {
+  simConf conf{r, tests / threads};
+  std::vector<pthread_t> th(threads);
+  std::vector<thrSt> st(threads);
+
+  for (size_t i = 0; i < threads; ++i) {
+    st[i] = {&conf, std::random_device{}() + i * 1000, 0};
+    int err = pthread_create(&th[i], nullptr, worker, &st[i]);
+    if (err) {
+      std::cerr << "create err: " << strerror(err) << "\n";[cite: 1]
+    }
+  }
+
+  size_t tot = 0;
+  for (size_t i = 0; i < threads; ++i) {
+    int err = pthread_join(th[i], nullptr);
+    if (err) {
+      std::cerr << "join err: " << strerror(err) << "\n";[cite: 1]
+    }
+    tot += st[i].s_h;
+  }
+
+  size_t rem = tests % threads;
+  if (rem > 0) {
+    tot += clac(r, rem, std::random_device{}());
+  }
+
+  return (static_cast<double>(tot) / tests) * (4.0 * r * r);
+}
+
 int main() {
 
 }
