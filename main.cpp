e4801c01feaf0fc754fc5975e68e69b51b32157b
Kirill <kirillpetrov56333@gmail.com>
2026-09-19 22:08:55 +0300

    adding calc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+#include <iostream>
+
 bool isInside(double x, double y, double r)
 {
   return (x * x + y * y <= r * r);
+}
+
+size_t clac(double r, size_t tests, size_t seed)
+{
+  std::default_random_engine eng(seed);
+  std::uniform_real_distribution<double> d(0, 2 * r);
+  size_t с = 0;
+  for (size_t i = 0; i < tests; ++i) {
+    с += isInside(dist(eng), dist(eng), r);
+  }
+  return с;
 }
 
 int main() {
